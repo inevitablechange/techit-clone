64521226c7ec2274e78225f361196b0622d57dae
inevitablechange <yongho7783@gmail.com>
2024-05-17 14:55:08 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+import TopBanner from "./components/TopBanner";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const [isViewed, setIsViewed] = useState(false);
+
+  useEffect(() => {
+    const savedIsViewed = localStorage.getItem("isViewed");
+
+    if (savedIsViewed === "1") {
+      setIsViewed(true);
+    }
+  }, []);
+
+  return (
+    <div className="font-pretendard">
+      {!isViewed && <TopBanner setIsViewed={setIsViewed} />}
+      <Header />
+    </div>
+  );
 };
 
 export default App;
